799c31484f18a7736dd980a7f10b9b77bdf7dcd6
mac <>
2020-11-01 23:08:19 +0700

    修改vue的操作

---
+++ shop/pages/index/index.vue
@@ -0,0 +1,56 @@
+<template>
+	<view class="container">
+		<!--搜索框-->
+		<view class="mp-search-box">
+			<input />
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+	
+	.mp-search-box{
+		position:absolute;
+	}
+</style>
