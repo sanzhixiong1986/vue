1fac6d56eca56125e2f1b5ece8a0dbd5e2018ff1
mac <>
2020-11-04 16:17:02 +0700

    修改了我的人物操作

--- uni-shop/pages/user/user.vue
+++ uni-shop/pages/user/user.vue
@@ -1,390 +1,374 @@
-<template>
-	<!-- 最外层操作 -->
-	<view class="container">
-		<view v-if="!hasLogin || empty === true" class="empty">
-			<image src="/static/emptyCart.jpg" mode="aspectFit"></image>
-			<view v-if="hasLogin" class="empty-tips">
-				空空如也
-				<navigator class="navigator" v-if="hasLogin" url="../index/index" open-type="switchTab">随便逛逛></navigator>
+<template>  
+    <view class="container">  
+		
+		<view class="user-section">
+			<image class="bg" src="/static/user-bg.jpg"></image>
+			<view class="user-info-box">
+				<view class="portrait-box">
+					<image class="portrait" src="/static/missing-face.png"></image>
+				</view>
+				<view class="info-box">
+					<text class="username">{{userInfo.nickname || '游客'}}</text>
+				</view>
 			</view>
-			<view v-else class="empty-tips">
-				空空如也
-				<view class="navigator" @click="navToLogin">去登陆></view>
+			<view class="vip-card-box">
+				<image class="card-bg" src="/static/vip-card-bg.png" mode=""></image>
+				<view class="b-btn">
+					立即开通
+				</view>
+				<view class="tit">
+					<text class="yticon icon-iLinkapp-"></text>
+					DCloud会员
+				</view>
+				<text class="e-m">DCloud Union</text>
+				<text class="e-b">开通会员开发无bug 一测就上线</text>
 			</view>
 		</view>
-		<view v-else>
-			<!-- 列表操作 -->
-			<view class="cart-list">
-				<block v-for="(item,index) in cartList" :key="item.id">
-					<view class="cart-item"
-						:class="{'b-b': index!==cartList.length-1}">
-							<view class="image-wrapper">
-								<image :src="item.image" mode="aspectFill"
-								:class="[item.loaded]" lazy-load
-								@load="onImageLoad('cartList', index)" 
-								@error="onImageError('cartList', index)">
-								</image>
-								<view 
-									class="yticon icon-xuanzhong2 checkbox"
-									:class="{checked: item.checked}"
-									@click="check('item', index)">
-								</view>
-							</view>
-							
-							<view class="item-right">
-								<text class="clamp title">{{item.title}}</text>
-								<text class="attr">{{item.attr_val}}</text>
-								<text class="price">¥{{item.price}}</text>
-								<uni-number-box 
-									class="step"
-									:min="1" 
-									:max="item.stock"
-									:value="item.number>item.stock?item.stock:item.number"
-									:isMax="item.number>=item.stock?true:false"
-									:isMin="item.number===1"
-									:index="index"
-									@eventChange="numberChange"
-								></uni-number-box>
-							</view>
-							
-							<text class="del-btn yticon icon-fork" @click="deleteCartItem(index)"></text>
-						</view>
-				</block>
-			</view>
+		
+		<view 
+			class="cover-container"
+			:style="[{
+				transform: coverTransform,
+				transition: coverTransition
+			}]"
+			@touchstart="coverTouchstart"
+			@touchmove="coverTouchmove"
+			@touchend="coverTouchend"
+		>
+			<image class="arc" src="/static/arc.png"></image>
 			
-			<!-- 底部菜单栏 -->
-			<view class="action-section">
-				<view class="checkbox">
-					<image 
-						:src="allChecked?'/static/selected.png':'/static/select.png'" 
-						mode="aspectFit"
-						@click="check('all')"
-					></image>
-					<view class="clear-btn" :class="{show: allChecked}" @click="clearCart">
-						清空
-					</view>
+			<view class="tj-sction">
+				<view class="tj-item">
+					<text class="num">128.8</text>
+					<text>余额</text>
 				</view>
-				<view class="total-box">
-					<text class="price">¥{{total}}</text>
-					<text class="coupon">
-						已优惠
-						<text>74.35</text>
-						元
-					</text>
+				<view class="tj-item">
+					<text class="num">0</text>
+					<text>优惠券</text>
 				</view>
-				<button type="primary" class="no-border confirm-btn" @click="createOrder">去结算</button>
+				<view class="tj-item">
+					<text class="num">20</text>
+					<text>积分</text>
+				</view>
+			</view>
+			<!-- 订单 -->
+			<view class="order-section">
+				<view class="order-item" @click="navTo('/pages/order/order?state=0')" hover-class="common-hover"  :hover-stay-time="50">
+					<text class="yticon icon-shouye"></text>
+					<text>全部订单</text>
+				</view>
+				<view class="order-item" @click="navTo('/pages/order/order?state=1')"  hover-class="common-hover" :hover-stay-time="50">
+					<text class="yticon icon-daifukuan"></text>
+					<text>待付款</text>
+				</view>
+				<view class="order-item" @click="navTo('/pages/order/order?state=2')" hover-class="common-hover"  :hover-stay-time="50">
+					<text class="yticon icon-yishouhuo"></text>
+					<text>待收货</text>
+				</view>
+				<view class="order-item" @click="navTo('/pages/order/order?state=4')" hover-class="common-hover"  :hover-stay-time="50">
+					<text class="yticon icon-shouhoutuikuan"></text>
+					<text>退款/售后</text>
+				</view>
+			</view>
+			<!-- 浏览历史 -->
+			<view class="history-section icon">
+				<view class="sec-header">
+					<text class="yticon icon-lishijilu"></text>
+					<text>浏览历史</text>
+				</view>
+				<scroll-view scroll-x class="h-list">
+					<image @click="navTo('/pages/product/product')" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553105186633&di=c121a29beece4e14269948d990f9e720&imgtype=0&src=http%3A%2F%2Fimg004.hc360.cn%2Fm8%2FM04%2FDE%2FDE%2FwKhQplZ-QteEBvsbAAAAADUkobU751.jpg" mode="aspectFill"></image>
+					<image @click="navTo('/pages/product/product')" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553105231218&di=09534b9833b5243296630e6d5b728eff&imgtype=0&src=http%3A%2F%2Fimg002.hc360.cn%2Fm1%2FM05%2FD1%2FAC%2FwKhQcFQ3iN2EQTo8AAAAAHQU6_8355.jpg" mode="aspectFill"></image>
+					<image @click="navTo('/pages/product/product')" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553105320890&di=c743386be51f2c4c0fd4b75754d14f3c&imgtype=0&src=http%3A%2F%2Fimg007.hc360.cn%2Fhb%2FMTQ1OTg4ODY0MDA3Ny05OTQ4ODY1NDQ%3D.jpg" mode="aspectFill"></image>
+					<image @click="navTo('/pages/product/product')" src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2691146630,2165926318&fm=26&gp=0.jpg" mode="aspectFill"></image>
+					<image @click="navTo('/pages/product/product')" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553105443324&di=8141bf13f3f208c61524d67f9bb83942&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01ac9a5548d29b0000019ae98e6d98.jpg" mode="aspectFill"></image>
+					<image @click="navTo('/pages/product/product')" src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=191678693,2701202375&fm=26&gp=0.jpg" mode="aspectFill"></image>
+				</scroll-view>
+				<list-cell icon="icon-iconfontweixin" iconColor="#e07472" title="我的钱包" tips="您的会员还有3天过期"></list-cell>
+				<list-cell icon="icon-dizhi" iconColor="#5fcda2" title="地址管理" @eventClick="navTo('/pages/address/address')"></list-cell>
+				<list-cell icon="icon-share" iconColor="#9789f7" title="分享" tips="邀请好友赢10万大礼"></list-cell>
+				<list-cell icon="icon-pinglun-copy" iconColor="#ee883b" title="晒单" tips="晒单抢红包"></list-cell>
+				<list-cell icon="icon-shoucang_xuanzhongzhuangtai" iconColor="#54b4ef" title="我的收藏"></list-cell>
+				<list-cell icon="icon-shezhi1" iconColor="#e07472" title="设置" border="" @eventClick="navTo('/pages/set/set')"></list-cell>
 			</view>
 		</view>
-	</view>
-</template>
+			
+		
+    </view>  
+</template>  
+<script>  
+	import listCell from '@/components/mix-list-cell';
+    import {  
+        mapState 
+    } from 'vuex';  
+	let startY = 0, moveY = 0, pageAtTop = true;
+    export default {
+		components: {
+			listCell
+		},
+		data(){
+			return {
+				coverTransform: 'translateY(0px)',
+				coverTransition: '0s',
+				moving: false,
+			}
+		},
+		onLoad(){
+		},
+		// #ifndef MP
+		onNavigationBarButtonTap(e) {
+			const index = e.index;
+			if (index === 0) {
+				this.navTo('/pages/set/set');
+			}else if(index === 1){
+				// #ifdef APP-PLUS
+				const pages = getCurrentPages();
+				const page = pages[pages.length - 1];
+				const currentWebview = page.$getAppWebview();
+				currentWebview.hideTitleNViewButtonRedDot({
+					index
+				});
+				// #endif
+				uni.navigateTo({
+					url: '/pages/notice/notice'
+				})
+			}
+		},
+		// #endif
+        computed: {
+			...mapState(['hasLogin','userInfo'])
+		},
+        methods: {
 
-<script>
-	import {
-			mapState
-		} from 'vuex';
-		import uniNumberBox from "@/components/uni-number-box/uni-number-box.vue"
-		export default {
-			components: {
-				uniNumberBox
+			/**
+			 * 统一跳转接口,拦截未登录路由
+			 * navigator标签现在默认没有转场动画，所以用view
+			 */
+			navTo(url){
+				if(!this.hasLogin){
+					url = '/pages/public/login';
+				}
+				uni.navigateTo({  
+					url
+				})  
+			}, 
+	
+			/**
+			 *  会员卡下拉和回弹
+			 *  1.关闭bounce避免ios端下拉冲突
+			 *  2.由于touchmove事件的缺陷（以前做小程序就遇到，比如20跳到40，h5反而好很多），下拉的时候会有掉帧的感觉
+			 *    transition设置0.1秒延迟，让css来过渡这段空窗期
+			 *  3.回弹效果可修改曲线值来调整效果，推荐一个好用的bezier生成工具 http://cubic-bezier.com/
+			 */
+			coverTouchstart(e){
+				if(pageAtTop === false){
+					return;
+				}
+				this.coverTransition = 'transform .1s linear';
+				startY = e.touches[0].clientY;
 			},
-			data() {
-				return {
-					total: 0, //总价格
-					allChecked: false, //全选状态  true|false
-					empty: false, //空白页现实  true|false
-					cartList: [],
-				};
-			},
-			onLoad(){
-				this.loadData();
-			},
-			watch:{
-				//显示空白页
-				cartList(e){
-					let empty = e.length === 0 ? true: false;
-					if(this.empty !== empty){
-						this.empty = empty;
-					}
+			coverTouchmove(e){
+				moveY = e.touches[0].clientY;
+				let moveDistance = moveY - startY;
+				if(moveDistance < 0){
+					this.moving = false;
+					return;
+				}
+				this.moving = true;
+				if(moveDistance >= 80 && moveDistance < 100){
+					moveDistance = 80;
+				}
+		
+				if(moveDistance > 0 && moveDistance <= 80){
+					this.coverTransform = `translateY(${moveDistance}px)`;
 				}
 			},
-			computed:{
-				...mapState(['hasLogin'])
-			},
-			methods: {
-				//请求数据
-				async loadData(){
-					let list = await this.$api.json('cartList'); 
-					let cartList = list.map(item=>{
-						item.checked = true;
-						return item;
-					});
-					this.cartList = cartList;
-					this.calcTotal();  //计算总价
-				},
-				//监听image加载完成
-				onImageLoad(key, index) {
-					this.$set(this[key][index], 'loaded', 'loaded');
-				},
-				//监听image加载失败
-				onImageError(key, index) {
-					this[key][index].image = '/static/errorImage.jpg';
-				},
-				navToLogin(){
-					uni.navigateTo({
-						url: '/pages/public/login'
-					})
-				},
-				 //选中状态处理
-				check(type, index){
-					if(type === 'item'){
-						this.cartList[index].checked = !this.cartList[index].checked;
-					}else{
-						const checked = !this.allChecked
-						const list = this.cartList;
-						list.forEach(item=>{
-							item.checked = checked;
-						})
-						this.allChecked = checked;
-					}
-					this.calcTotal(type);
-				},
-				//数量
-				numberChange(data){
-					this.cartList[data.index].number = data.number;
-					this.calcTotal();
-				},
-				//删除
-				deleteCartItem(index){
-					let list = this.cartList;
-					let row = list[index];
-					let id = row.id;
-	
-					this.cartList.splice(index, 1);
-					this.calcTotal();
-					uni.hideLoading();
-				},
-				//清空
-				clearCart(){
-					uni.showModal({
-						content: '清空购物车？',
-						success: (e)=>{
-							if(e.confirm){
-								this.cartList = [];
-							}
-						}
-					})
-				},
-				//计算总价
-				calcTotal(){
-					let list = this.cartList;
-					if(list.length === 0){
-						this.empty = true;
-						return;
-					}
-					let total = 0;
-					let checked = true;
-					list.forEach(item=>{
-						if(item.checked === true){
-							total += item.price * item.number;
-						}else if(checked === true){
-							checked = false;
-						}
-					})
-					this.allChecked = checked;
-					this.total = Number(total.toFixed(2));
-				},
-				//创建订单
-				createOrder(){
-					let list = this.cartList;
-					let goodsData = [];
-					list.forEach(item=>{
-						if(item.checked){
-							goodsData.push({
-								attr_val: item.attr_val,
-								number: item.number
-							})
-						}
-					})
-	
-					uni.navigateTo({
-						url: `/pages/order/createOrder?data=${JSON.stringify({
-							goodsData: goodsData
-						})}`
-					})
-					this.$api.msg('跳转下一页 sendData');
+			coverTouchend(){
+				if(this.moving === false){
+					return;
 				}
+				this.moving = false;
+				this.coverTransition = 'transform 0.3s cubic-bezier(.21,1.93,.53,.64)';
+				this.coverTransform = 'translateY(0px)';
+			}
+        }  
+    }  
+</script>  
+<style lang='scss'>
+	%flex-center {
+	 display:flex;
+	 flex-direction: column;
+	 justify-content: center;
+	 align-items: center;
+	}
+	%section {
+	  display:flex;
+	  justify-content: space-around;
+	  align-content: center;
+	  background: #fff;
+	  border-radius: 10upx;
+	}
+
+	.user-section{
+		height: 520upx;
+		padding: 100upx 30upx 0;
+		position:relative;
+		.bg{
+			position:absolute;
+			left: 0;
+			top: 0;
+			width: 100%;
+			height: 100%;
+			filter: blur(1px);
+			opacity: .7;
+		}
+	}
+	.user-info-box{
+		height: 180upx;
+		display:flex;
+		align-items:center;
+		position:relative;
+		z-index: 1;
+		.portrait{
+			width: 130upx;
+			height: 130upx;
+			border:5upx solid #fff;
+			border-radius: 50%;
+		}
+		.username{
+			font-size: $font-lg + 6upx;
+			color: $font-color-dark;
+			margin-left: 20upx;
+		}
+	}
+
+	.vip-card-box{
+		display:flex;
+		flex-direction: column;
+		color: #f7d680;
+		height: 240upx;
+		background: linear-gradient(left, rgba(0,0,0,.7), rgba(0,0,0,.8));
+		border-radius: 16upx 16upx 0 0;
+		overflow: hidden;
+		position: relative;
+		padding: 20upx 24upx;
+		.card-bg{
+			position:absolute;
+			top: 20upx;
+			right: 0;
+			width: 380upx;
+			height: 260upx;
+		}
+		.b-btn{
+			position: absolute;
+			right: 20upx;
+			top: 16upx;
+			width: 132upx;
+			height: 40upx;
+			text-align: center;
+			line-height: 40upx;
+			font-size: 22upx;
+			color: #36343c;
+			border-radius: 20px;
+			background: linear-gradient(left, #f9e6af, #ffd465);
+			z-index: 1;
+		}
+		.tit{
+			font-size: $font-base+2upx;
+			color: #f7d680;
+			margin-bottom: 28upx;
+			.yticon{
+				color: #f6e5a3;
+				margin-right: 16upx;
 			}
 		}
-</script>
-
-<style lang="scss">
-	.container{
-			padding-bottom: 134upx;
-			/* 空白页 */
-			.empty{
-				position:fixed;
-				left: 0;
-				top:0;
-				width: 100%;
-				height: 100vh;
-				padding-bottom:100upx;
-				display:flex;
-				justify-content: center;
-				flex-direction: column;
-				align-items:center;
-				background: #fff;
-				image{
-					width: 240upx;
-					height: 160upx;
-					margin-bottom:30upx;
-				}
-				.empty-tips{
-					display:flex;
-					font-size: $font-sm+2upx;
-					color: $font-color-disabled;
-					.navigator{
-						color: $uni-color-primary;
-						margin-left: 16upx;
-					}
-				}
-			}
+		.e-b{
+			font-size: $font-sm;
+			color: #d8cba9;
+			margin-top: 10upx;
 		}
-		/* 购物车列表项 */
-		.cart-item{
+	}
+	.cover-container{
+		background: $page-color-base;
+		margin-top: -150upx;
+		padding: 0 30upx;
+		position:relative;
+		background: #f5f5f5;
+		padding-bottom: 20upx;
+		.arc{
+			position:absolute;
+			left: 0;
+			top: -34upx;
+			width: 100%;
+			height: 36upx;
+		}
+	}
+	.tj-sction{
+		@extend %section;
+		.tj-item{
+			@extend %flex-center;
+			flex-direction: column;
+			height: 140upx;
+			font-size: $font-sm;
+			color: #75787d;
+		}
+		.num{
+			font-size: $font-lg;
+			color: $font-color-dark;
+			margin-bottom: 8upx;
+		}
+	}
+	.order-section{
+		@extend %section;
+		padding: 28upx 0;
+		margin-top: 20upx;
+		.order-item{
+			@extend %flex-center;
+			width: 120upx;
+			height: 120upx;
+			border-radius: 10upx;
+			font-size: $font-sm;
+			color: $font-color-dark;
+		}
+		.yticon{
+			font-size: 48upx;
+			margin-bottom: 18upx;
+			color: #fa436a;
+		}
+		.icon-shouhoutuikuan{
+			font-size:44upx;
+		}
+	}
+	.history-section{
+		padding: 30upx 0 0;
+		margin-top: 20upx;
+		background: #fff;
+		border-radius:10upx;
+		.sec-header{
 			display:flex;
-			position:relative;
-			padding:30upx 40upx;
-			.image-wrapper{
-				width: 230upx;
-				height: 230upx;
-				flex-shrink: 0;
-				position:relative;
-				image{
-					border-radius:8upx;
-				}
-			}
-			.checkbox{
-				position:absolute;
-				left:-16upx;
-				top: -16upx;
-				z-index: 8;
-				font-size: 44upx;
-				line-height: 1;
-				padding: 4upx;
-				color: $font-color-disabled;
-				background:#fff;
-				border-radius: 50px;
-			}
-			.item-right{
-				display:flex;
-				flex-direction: column;
-				flex: 1;
-				overflow: hidden;
-				position:relative;
-				padding-left: 30upx;
-				.title,.price{
-					font-size:$font-base + 2upx;
-					color: $font-color-dark;
-					height: 40upx;
-					line-height: 40upx;
-				}
-				.attr{
-					font-size: $font-sm + 2upx;
-					color: $font-color-light;
-					height: 50upx;
-					line-height: 50upx;
-				}
-				.price{
-					height: 50upx;
-					line-height:50upx;
-				}
-			}
-			.del-btn{
-				padding:4upx 10upx;
-				font-size:34upx; 
-				height: 50upx;
-				color: $font-color-light;
-			}
-		}
-		/* 底部栏 */
-		.action-section{
-			/* #ifdef H5 */
-			margin-bottom:100upx;
-			/* #endif */
-			position:fixed;
-			left: 30upx;
-			bottom:30upx;
-			z-index: 95;
-			display: flex;
 			align-items: center;
-			width: 690upx;
-			height: 100upx;
-			padding: 0 30upx;
-			background: rgba(255,255,255,.9);
-			box-shadow: 0 0 20upx 0 rgba(0,0,0,.5);
-			border-radius: 16upx;
-			.checkbox{
-				height:52upx;
-				position:relative;
-				image{
-					width: 52upx;
-					height: 100%;
-					position:relative;
-					z-index: 5;
-				}
-			}
-			.clear-btn{
-				position:absolute;
-				left: 26upx;
-				top: 0;
-				z-index: 4;
-				width: 0;
-				height: 52upx;
-				line-height: 52upx;
-				padding-left: 38upx;
-				font-size: $font-base;
-				color: #fff;
-				background: $font-color-disabled;
-				border-radius:0 50px 50px 0;
-				opacity: 0;
-				transition: .2s;
-				&.show{
-					opacity: 1;
-					width: 120upx;
-				}
-			}
-			.total-box{
-				flex: 1;
-				display:flex;
-				flex-direction: column;
-				text-align:right;
-				padding-right: 40upx;
-				.price{
-					font-size: $font-lg;
-					color: $font-color-dark;
-				}
-				.coupon{
-					font-size: $font-sm;
-					color: $font-color-light;
-					text{
-						color: $font-color-dark;
-					}
-				}
-			}
-			.confirm-btn{
-				padding: 0 38upx;
-				margin: 0;
-				border-radius: 100px;
-				height: 76upx;
-				line-height: 76upx;
-				font-size: $font-base + 2upx;
-				background: $uni-color-primary;
-				box-shadow: 1px 2px 5px rgba(217, 60, 93, 0.72)
+			font-size: $font-base;
+			color: $font-color-dark;
+			line-height: 40upx;
+			margin-left: 30upx;
+			.yticon{
+				font-size: 44upx;
+				color: #5eba8f;
+				margin-right: 16upx;
+				line-height: 40upx;
 			}
 		}
-		/* 复选框选中状态 */
-		.action-section .checkbox.checked,
-		.cart-item .checkbox.checked{
-			color: $uni-color-primary;
+		.h-list{
+			white-space: nowrap;
+			padding: 30upx 30upx 0;
+			image{
+				display:inline-block;
+				width: 160upx;
+				height: 160upx;
+				margin-right: 20upx;
+				border-radius: 10upx;
+			}
 		}
+	}
+	
 </style>
