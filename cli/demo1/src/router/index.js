b3629686b8ea413a5efa239d92d2ce0f6ee67dea
sanzhixiong1986 <57082213+sanzhixiong1986@users.noreply.github.com>
2020-11-21 10:43:18 +0800

    第一次提交vue-cil操作

---
+++ cli/demo1/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+// 給出對應的渲染路徑
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
